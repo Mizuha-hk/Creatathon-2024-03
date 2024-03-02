950c3d90532b98c309d254af8305b3440f185dbc
Miz ha <hk.haru0803@gmail.com>
2024-03-02 13:41:11 +0900

    [add] #4 GlobalUsingsの追加

---
+++ TranslatorMobile/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.CognitiveServices.Speech;
+global using Microsoft.CognitiveServices.Speech.Translation;
+global using Microsoft.Extensions.Configuration;
+
+global using TranslatorShared;
