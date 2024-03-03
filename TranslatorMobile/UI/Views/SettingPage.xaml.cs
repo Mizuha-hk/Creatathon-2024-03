1ebe06dd074ab306ab3d1aae7f8766c27c443c16
Miz ha <hk.haru0803@gmail.com>
2024-03-03 12:40:26 +0900

    [modify] #12 Viewの追加

--- TranslatorMobile/UI/Views/SettingPage.xaml.cs
+++ TranslatorMobile/UI/Views/SettingPage.xaml.cs
@@ -1,9 +1,16 @@
+using TranslatorMobile.UI.ViewModels;
+
 namespace TranslatorMobile.UI.Views;
 
 public partial class SettingPage : ContentPage
 {
+	public string SubscriptionKey { get; set; }
+	public string Region { get; set; }
+
+
 	public SettingPage()
 	{
 		InitializeComponent();
+		BindingContext = new SettingsViewModel();
 	}
 }
